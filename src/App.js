ebf543c4812cc9058cea7c2600dd81e49ba9359f
Mohd. Shayan <70843731+MohdShayan@users.noreply.github.com>
2024-10-06 19:39:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 // App.js
-import './App.css';
-import Form from './components/Myform.js';
-import Navbar from './components/Navbar.js';
+import "./App.css";
+import Form from "./components/Myform.js";
+import Navbar from "./components/Navbar.js";
 import { Box, useColorModeValue } from "@chakra-ui/react";
 
 function App() {
-  const bgColor = useColorModeValue("#EEEAE3","#212121")
+  const bgColor = useColorModeValue("#EEEAE3", "#212121");
   return (
-      <Box 
-      minH="100vh"          
-      h="auto"              
-      w="100%"              
-      maxW="100vw"         
-      bg={bgColor}          
-      overflowY="auto"      
-      overflowX="hidden"    
+    <Box
+      minH="100vh"
+      h="full"
+      w="100%"
+      maxW="100vw"
+      bg={bgColor}
+      overflowY={{ base: "auto", md: "hidden" }}
+      overflowX="hidden"
+      display="flex"
+      flexDirection="column"
     >
       <Navbar />
-      <Form />
-  </Box>
+      <Box flex="1" overflowY="auto">
+        <Box overflowX="auto">
+          <Form />
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
